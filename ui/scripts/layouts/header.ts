649b1b4f7cc18357d64d05b3f40ad14d87458088
furry-dev <rudenkomax20072017@gmail.com>
2024-01-03 21:02:20 +0200

    Add search results in UI and add adaptive for small window size

--- ui/scripts/layouts/header.ts
+++ ui/scripts/layouts/header.ts
@@ -1,12 +1,28 @@
 import { appWindow } from '@tauri-apps/api/window'
+import { exit } from '@tauri-apps/api/process'
+import { ask } from '@tauri-apps/api/dialog'
 
 const minimizeButton = document.getElementById('minimize-window')
 const closeButton = document.getElementById('close-window')
+const exitButton = document.getElementById('exit-app')
+const showMoreWindows = document.getElementById("show-more-windows")
 
-if (minimizeButton) {
-    minimizeButton.addEventListener('click', () => appWindow.minimize())
-}
+minimizeButton?.addEventListener('click', () => appWindow.minimize())
 
-if (closeButton) {
-    closeButton.addEventListener('click', () => appWindow.close())
-}
+closeButton?.addEventListener('click', () => {
+    if (!closeButton.dataset.confirm) return appWindow.close()
+
+    if (confirm("Закрыть окно?")) {
+        void appWindow.close()
+    }
+})
+
+exitButton?.addEventListener('click', () => {
+    ask("Вы хотите выйти из приложения?", { type: 'warning' }).then((status) => {
+        if (status) void exit()
+    })
+})
+
+showMoreWindows?.addEventListener('click', () => {
+    document.getElementById('more-windows')?.classList.toggle('active')
+})
---
+++ ui/scripts/windows/main.ts
@@ -0,0 +1,10 @@
+// open folder and get folder path
+
+// import { open } from '@tauri-apps/api/dialog'
+
+// const selected = await open({
+//     directory: true,
+//     multiple: true
+//   })
+
+// console.log(selected)
