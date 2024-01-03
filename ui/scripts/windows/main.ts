e719babc619c9ee18a77de73fe6ec699d58349a1
furry-dev <rudenkomax20072017@gmail.com>
2024-01-03 23:06:46 +0200

    add more adaptive and hide results on search input

--- ui/scripts/windows/main.ts
+++ ui/scripts/windows/main.ts
@@ -8,3 +8,33 @@
 //   })
 
 // console.log(selected)
+
+
+const soundSearch = document.querySelector('#sound-page .header .search')
+const soundInput = soundSearch?.querySelector('form input')
+const soundResults = document.querySelector('#sound-page .results .result-list')
+
+soundSearch?.addEventListener('click', (e) => e.stopPropagation())
+
+const disableSearch = (e: Event) => {
+    e.preventDefault()
+    soundSearch?.classList.remove('active')
+    soundResults?.classList.remove("search")
+}
+
+soundSearch?.querySelector('form')?.addEventListener('submit', (e) => {
+    disableSearch(e)
+})
+
+soundSearch?.addEventListener('keydown', (e) => {
+    const key = (e as KeyboardEvent).key
+
+    if (key === "Escape") {
+        disableSearch(e)
+    }
+})
+
+soundInput?.addEventListener('click', () => {
+    soundSearch?.classList.add('active')
+    soundResults?.classList.add("search")
+})
